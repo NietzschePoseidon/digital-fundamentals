01761fe593fa7cec9593511418c4509102b46963
Ellis <147318799+NietzschePoseidon@users.noreply.github.com>
2025-11-07 13:55:44 +0300

    Create analyser2.sh

---
+++ scripts/analyser2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+path=~/DFM/LAB04/
+
+for i in $(ls $path)
+do
+  for file in $(cat $path$i)
+  do
+    mail=$(echo $file | cut -d ":" -f"3")
+    if [[ $mail =~ "@spbstu.ru$" ]]
+    then
+      echo -e "Почта: $mail"
+    fi
+  done
+done
