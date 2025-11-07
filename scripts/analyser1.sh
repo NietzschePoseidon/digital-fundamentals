2ab353d36dc928b975f3e2c40bc18b3fb7e0c008
Ellis <147318799+NietzschePoseidon@users.noreply.github.com>
2025-11-07 13:54:27 +0300

    Update analyser1.sh

--- scripts/analyser1.sh
+++ scripts/analyser1.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+path=~/DFM/LAB04/
+
+for i in $(ls $path)
+do
+  for file in $(cat $path$i)
+  do
+    name=$(echo $file | cut -d ":" -f"1")
+    age=$(echo $file | cut -d ":" -f"2")
+    mail=$(echo $file | cut -d ":" -f"3")
+    echo -e "Имя: $name, возраст: $age, почта: $mail"
+  done
+done
